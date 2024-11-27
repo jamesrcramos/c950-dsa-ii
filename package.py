3f8916250e343aa4422b2387c061006af0326092
jamesrcramos <jamesrcramos@gmail.com>
2024-11-27 10:41:48 -0500

    Initial commit

---
+++ package.py
@@ -0,0 +1,12 @@
+class Package:
+    def __init__(self):
+        self.id = None
+        self.address = None
+        self.city = None
+        self.state = None
+        self.zip = None
+        self.delivery_deadline = None
+        self.weight = None
+        self.special_notes = None
+        self.delivery_status = "At Hub"
+        self.delivery_time = None
